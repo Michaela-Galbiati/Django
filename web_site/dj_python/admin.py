71deed2104a9e9f13986e7d89d34e4b00adda700
Michaela Galbiati <118918758+Michaela-Galbiati@users.noreply.github.com>
2023-05-23 10:17:59 -0300

    Initial commit

---
+++ web_site/dj_python/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from dj_python.models import Post
+
+admin.site.register(Post)
+
+# Register your models here.
---
+++ web_site/dj_python/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjPythonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dj_python'
